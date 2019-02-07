b5cb85a2f4207eefe03970b1543083ae9d363e52
CosignMyCodesign <dillonwilliamsail@gmail.com>
2019-02-07 16:16:17 -0600

    fixed migrations

--- website/models.py
+++ website/models.py
@@ -1,18 +1,52 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+# Customer Model
+
+class Customer(models.Model):
+    '''This Model defines all of the relationships and attributes of the Customer table.
+
+        Author: Group
+
+        Methods:
+            __str__ -- user
+    '''
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    address = models.CharField(max_length= 200)
+    phoneNumber = models.CharField(max_length= 12)
+    deletedOn = models.DateField(default = None)
+
+    def __str__(self):
+        ''' returns a string representation of the model '''
+        return self.user
+
+# Product Type
+class ProductType(models.Model):
+    '''Defines a model for the relationships and attributes of the ProductType table
+
+        Author: Group
+
+        Methods:
+            __str__ -- name
+    '''
+    name = models.CharField(max_length = 100)
+    deletedOn = models.DateField(default = None)
+
+    def __str__(self):
+        ''' returns a string representation of the model '''
+        return self.name
 
 # Product Model
 
 class Product(models.Model):
-'''This Model defines all the relationships and attributes of the Products table in our
-    database.
+    '''This Model defines all the relationships and attributes of the Products table in our
+        database.
 
-    Author: Group
+        Author: Group
 
-    Methods:
+        Methods:
         __str__ -- title
-'''
+    '''
     customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
     productType = models.ForeignKey(ProductType, on_delete= models.PROTECT)
     title = models.CharField(max_length=255)
@@ -25,35 +59,17 @@ class Product(models.Model):
         ''' returns a string representation of the model '''
         return self.title
 
-# Customer Model
-
-class Customer(models.Model):
-'''This Model defines all of the relationships and attributes of the Customer table.
-
-    Author: Group
-
-    Methods:
-        __str__ -- user
-'''
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    address = models.CharField(max_length= 200)
-    phoneNumber = models.CharField(max_length= 12)
-    deletedOn = models.DateField(default = None)
-
-    def __str__(self)
-        ''' returns a string representation of the model '''
-        return self.user
 
 # Payment Type Model
 
 class PaymentType(models.Model):
-'''Defines a model for the relationships and attributes for the Payment Table.
+    '''Defines a model for the relationships and attributes for the Payment Table.
 
-    Author: Group
+        Author: Group
 
-    Methods:
-        __str__ -- name
-'''
+        Methods:
+            __str__ -- name
+    '''
     name = models.CharField(max_length= 50)
     accountNumber = models.IntegerField()
     customer = models.ForeignKey(Customer, on_delete= models.PROTECT)
@@ -66,13 +82,13 @@ class PaymentType(models.Model):
 # Order Model
 
 class Order(models.Model):
-'''Defines a model for a customer order and includes customers payment info
+    '''Defines a model for a customer order and includes customers payment info
 
-    Author: Group
+        Author: Group
 
-    Methods:
-        __str__ -- id
-'''
+        Methods:
+            __str__ -- id
+    '''
 
     customer = models.ForeignKey(Customer, on_delete = models.PROTECT)
     paymentType = models.ForeignKey(PaymentType, on_delete = models.PROTECT)
@@ -87,35 +103,19 @@ class Order(models.Model):
 # Product Order
 
 class ProductOrder(models.Model):
-''' Defines a model for the connection between Product and Order models
+    ''' Defines a model for the connection between Product and Order models
 
-    Author: Group
+        Author: Group
 
-    Methods:
-        __str__ -- order, product
-'''
+        Methods:
+            __str__ -- order, product
+    '''
 
-    order = models.ForeignKey(Order)
-    product = models.ForeignKey(Product)
+    order = models.ForeignKey(Order, on_delete = models.PROTECT)
+    product = models.ForeignKey(Product, on_delete = models.PROTECT)
     deletedOn = models.DateField(default = None)
 
     def __str__(self):
         ''' returns a string representation of the model '''
         return self.order, self.product
 
-# Product Type
-
-class ProductType(models.Model):
-'''Defines a model for the relationships and attributes of the ProductType table
-
-    Author: Group
-
-    Methods:
-        __str__ -- name
-'''
-    name = models.CharField(max_length = 100)
-    deletedOn = models.DateField(default = None)
-
-    def __str__(self):
-        ''' returns a string representation of the model '''
-        return self.name
---
+++ website/views/__init__.py
@@ -0,0 +1,1 @@
+from .views import index, register, login_user, user_logout, sell_product, list_products
