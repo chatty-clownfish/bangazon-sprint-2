79b750c7f55c7b4ee3828aadb236c883116cfd6c
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2019-02-07 15:38:36 -0600

    doc strings added

--- website/ models.py
+++ website/ models.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Product Model
+
 class Product(models.Model):
+'''This Model defines all the relationships and attributes of the Products table in our
+    database.
+
+    Author: Group
+
+    Methods:
+        __str__ -- title
+'''
     customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
     productType = models.ForeignKey(ProductType, on_delete= models.PROTECT)
     title = models.CharField(max_length=255)
@@ -16,7 +26,15 @@ class Product(models.Model):
         return self.title
 
 # Customer Model
+
 class Customer(models.Model):
+'''This Model defines all of the relationships and attributes of the Customer table.
+
+    Author: Group
+
+    Methods:
+        __str__ -- user
+'''
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     address = models.CharField(max_length= 200)
     phoneNumber = models.CharField(max_length= 12)
@@ -27,7 +45,15 @@ class Customer(models.Model):
         return self.user
 
 # Payment Type Model
+
 class PaymentType(models.Model):
+'''Defines a model for the relationships and attributes for the Payment Table.
+
+    Author: Group
+
+    Methods:
+        __str__ -- name
+'''
     name = models.CharField(max_length= 50)
     accountNumber = models.IntegerField()
     customer = models.ForeignKey(Customer, on_delete= models.PROTECT)
@@ -38,7 +64,16 @@ class PaymentType(models.Model):
         return self.name
 
 # Order Model
+
 class Order(models.Model):
+'''Defines a model for a customer order and includes customers payment info
+
+    Author: Group
+
+    Methods:
+        __str__ -- id
+'''
+
     customer = models.ForeignKey(Customer, on_delete = models.PROTECT)
     paymentType = models.ForeignKey(PaymentType, on_delete = models.PROTECT)
     isCompleted = models.BooleanField(default = False)
@@ -50,7 +85,16 @@ class Order(models.Model):
         return self.id
 
 # Product Order
+
 class ProductOrder(models.Model):
+''' Defines a model for the connection between Product and Order models
+
+    Author: Group
+
+    Methods:
+        __str__ -- order, product
+'''
+
     order = models.ForeignKey(Order)
     product = models.ForeignKey(Product)
     deletedOn = models.DateField(default = None)
@@ -60,7 +104,15 @@ class ProductOrder(models.Model):
         return self.order, self.product
 
 # Product Type
+
 class ProductType(models.Model):
+'''Defines a model for the relationships and attributes of the ProductType table
+
+    Author: Group
+
+    Methods:
+        __str__ -- name
+'''
     name = models.CharField(max_length = 100)
     deletedOn = models.DateField(default = None)
 
