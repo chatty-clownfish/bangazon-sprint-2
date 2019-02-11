3f91c962c72dcbbdd45733d070817ee632f32bf4
Daniel Combs <danielreedcombs@gmail.com>
2019-02-11 10:45:31 -0600

    my products querys only that persons products now.

--- website/views/views.py
+++ website/views/views.py
@@ -90,9 +90,11 @@ def user_logout(request):
     # in the URL in redirects?????
     return HttpResponseRedirect('/')
 
-
+@login_required
 def list_products(request):
-    all_products = Product.objects.all()
+    user_id= request.user.id
+    all_products = Product.objects.raw('''select * from website_product
+    where customer_id = %s ''',[user_id])
     template_name = 'product/list.html'
     return render(request, template_name, {'products': all_products})
 
