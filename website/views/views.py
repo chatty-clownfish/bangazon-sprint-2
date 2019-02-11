1af047a9cb0ab4f195ac29ade5625892f85f2be9
robbyhecht <robbyhecht@gmail.com>
2019-02-11 12:29:42 -0600

    working on getting login required to eventually send user to requested page after logging in. also minor changes to navbar in that sell product will be visible

--- website/views/views.py
+++ website/views/views.py
@@ -58,8 +58,10 @@ def login_user(request):
 
     # Obtain the context for the user's request.
     context = RequestContext(request)
+    next_route = {"next": request.GET['next']}
 
     # If the request is a HTTP POST, try to pull out the relevant information.
+
     if request.method == 'POST':
 
         # Use the built-in authenticate method to verify
@@ -70,7 +72,7 @@ def login_user(request):
         # If authentication was successful, log the user in
         if authenticated_user is not None:
             login(request=request, user=authenticated_user)
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect(request.GET.get('next'))
 
         else:
             # Bad login details were provided. So we can't log the user in.
@@ -78,7 +80,7 @@ def login_user(request):
             return HttpResponse("Invalid login details supplied.")
 
 
-    return render(request, 'login.html', {}, context)
+    return render(request, 'login.html', next_route, context)
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
