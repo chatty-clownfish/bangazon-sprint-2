f68989ac1552b348b7095c6625b5e4b4407df9a4
robbyhecht <robbyhecht@gmail.com>
2019-02-11 20:25:26 -0600

    added login decorator to 'my products', moved all product list by type functionality to a new view called product_type_view and made necessary alterations to init and views.py.

---
+++ website/views/product_type_view.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from website.models import Product, ProductType
+
+def products_by_type(request, pk):
+    '''
+    This method gets one individual producttype and lists it along with all associated products by name, quantity and price.
+
+    Method uses a SELECT from the product table and JOIN with the producttype table.
+    '''
+    try:
+
+        sql = '''
+        SELECT p.title, p.quantity, p.price, pt.name, pt.id, p.id
+        FROM website_product p
+        JOIN website_producttype pt on pt.id = p.productType_id
+        WHERE pt.id=%s
+        '''
+        product_type = ProductType.objects.raw(sql, [pk])[0]
+        product = Product.objects.raw(sql, [pk])
+
+    except IndexError:
+        raise Http404("This product type does not exist")
+
+    context = {'type': product_type, 'prod':product}
+    return render(request, 'product/products_by_type.html', context)
