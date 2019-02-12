469bacd271336919b06d7ee1fe6db2fe44195bd4
CosignMyCodesign <dillonwilliamsail@gmail.com>
2019-02-12 12:42:46 -0600

    added a conditional in the profile list.html based on whether the user had paymenttypes. Also refactored the profilelist view to make things work a bit better

--- website/views/profile_list_view.py
+++ website/views/profile_list_view.py
@@ -6,7 +6,7 @@ from django.template import RequestContext
 from django.urls import reverse
 
 from website.forms import UserForm, ProductForm, AddPaymentForm
-from website.models import Product
+from website.models import Product, PaymentType
 from django.contrib.auth.models import User
 from django.db import connection
 
@@ -14,11 +14,11 @@ from django.db import connection
 def profileList(request):
 
     with connection.cursor() as cursor:
+        
         try:
             cursor.execute('''
                             SELECT * FROM auth_user AS a 
                             JOIN website_customer AS c ON a.id  = c.user_id
-                            JOIN website_paymenttype as p ON a.id = p.customer_id
                             WHERE a.id = %s
                             ''', [request.user.id])
 
@@ -32,12 +32,21 @@ def profileList(request):
                 to_add = dict(zip(columns, row))
                 profile.update(to_add)
 
+            try:
+                customerId = request.user.customer.id
+                print("CUSTOMER ID****", customerId)
+                sql = PaymentType.objects.raw('''SELECT * FROM website_paymenttype as p
+                                                WHERE p.customer_id == %s
+                                                ''', [customerId])[0]
+                print("SQLLLLLL", sql)
+            except:
+                sql = ""                                         
         except connection.OperationalError as err:
             print("Error...", err)
     
     print("CHECK THIS:", profile)
     
-    context = {"profile": profile}
+    context = {"profile": profile, "sql":sql}
     
     return render(request, 'profile/list.html', context)
 
