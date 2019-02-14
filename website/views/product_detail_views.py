286ebabebd1198b2d12d310023074e69671ffad3
John Wood <johnnathanwood@gmail.com>
2019-02-14 09:38:36 -0600

    changed product view detail into rawsql to grab the count of the remaining products available for customers to buy. If a product is in a customers cart, it will reflect as -1 in the product quantity

--- website/views/product_detail_views.py
+++ website/views/product_detail_views.py
@@ -11,6 +11,31 @@ from website.models import Product
 # title, description, quantity available, price/unit, and a button labeled Add to Order
 
 def product_details(request,id ):
-  product_details = get_object_or_404(Product, pk=id)
-  context = {'product_details' : product_details}
-  return render(request, 'product/productDetails.html', context)
+    user_id = request.user.id
+    product_details = get_object_or_404(Product, pk=id)
+    product_details_quantity = '''
+                                SELECT p.id, p.title, (p.quantity - COUNT(po.product_id)) as pCount
+                                FROM website_product p
+                                JOIN website_productorder po on po.product_id = p.id
+                                JOIN website_order o on o.id = po.order_id
+                                WHERE p.id = %s
+                               '''
+    product_details_quantity_id = Product.objects.raw(product_details_quantity, [id,])[0]
+    context = {'product_details' : product_details , 'user_id' : user_id, 'product_details_quantity_id': product_details_quantity_id}
+    return render(request, 'product/productDetails.html', context)
+
+
+def add_product_to_cart(request, product_details):
+    user_id = request.user.id
+    website_order_id = Order.objects.raw('''select * from website_order
+                                            Where deletedOn is null and customer_id = %s''', [user_id])[0]
+    print('website_order_id', website_order_id.id)
+    print('user_id', user_id)
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute('''Insert into website_productorder (deletedOn, order_id, product_id)
+                            Values(NULL, %s, %s)''', [website_order_id.id, product_details])
+        except IndexError:
+            raise Http404("This product type does not exist")
+
+    return HttpResponseRedirect(reverse('website:cart'))
